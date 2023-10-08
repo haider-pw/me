13c6d705c8593c4d19f53dc60a8d6680ace8f574
Syed Haider Hassan <haideritx@gmail.com>
2023-10-08 21:00:35 +0500

    Learning nx

--- apps/server/api/vercel.ts
+++ apps/server/api/vercel.ts
@@ -1,8 +1,16 @@
-import { getInitializedApp } from '../src';  // Adjust the path accordingly
-import express, { Request, Response } from 'express';
-import { VercelRequest, VercelResponse } from '@vercel/node';
+const app = require('express')();
+const { v4 } = require('uuid');
 
-module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  const app: express.Express = await getInitializedApp();
-  app(req, res);
-};
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
